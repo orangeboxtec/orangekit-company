ab5f9531305a05d3fc0c0cdc30c8cf5389af88cf
Bianca <biancabessa99@hotmail.com>
2024-01-08 14:08:21 -0300

    Version change

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "com.orangebox.kit.company"
-version = "1.0.0"
+version = "1.0.1"
 
 repositories {
     mavenCentral()
--- src/main/kotlin/com/orangebox/kit/company/CompanyCard.kt
+++ src/main/kotlin/com/orangebox/kit/company/CompanyCard.kt
@@ -1,5 +1,6 @@
 package com.orangebox.kit.company
 
+import com.orangebox.kit.company.cnae.Cnae
 import com.orangebox.kit.core.address.AddressInfo
 
 class CompanyCard {
@@ -12,4 +13,5 @@ class CompanyCard {
     var distance: Double? = null
     var addressInfo: AddressInfo? = null
     var info: Map<String, Any>? = null
+    var secondaryCnae: Cnae? = null
 }
